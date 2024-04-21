9b4b8532a3acbc8aeb6877c6507b1fcce5f6e4ee
Abhiram <abhiram.ch418@gmail.com>
2024-04-21 18:32:55 +0530

    added_bgimag_login

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  ShowCode:boolean=false;
+
+  constructor(private router:Router){}
   
+  code(){
+    this.ShowCode = !this.ShowCode;
+  }
+
+  SignUp(){
+    this.router.navigate(['signup']);
+  }
+
 }
---
+++ src/assets/links.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LinksService {
+
+  ApMap: string = "https://drive.google.com/file/d/1C3GhfFxuFlzetLPX9aa-agJoy6mfHnu9/view?usp=sharing";
+  TsMap: string = "https://drive.google.com/file/d/1d9KKyIvwqjS0O_Hx91g5w6480n27lr5g/view?usp=sharing";
+
+  constructor() { }
+
+}
