30e4b92b19ee3f3ff14d943ba959f5a5472c54ff
Abhiram <abhiram.ch418@gmail.com>
2024-04-28 03:15:00 +0530

    edited sign-up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { SigninUpPageComponent } from './signin-up-page/signin-up-page.component';
 import { DiceComponent } from './dice/dice.component';
 import { LoaderComponent } from './components/loader/loader.component';
+import { FooterComponent } from './footer/footer.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'comingSoon', pathMatch:'full'},
@@ -11,6 +12,8 @@ export const routes: Routes = [
     {path:'login', component:LoginPageComponent},
     {path:'signup', component:SigninUpPageComponent},
     
+    {path:'footer', component:FooterComponent},
+
     {path:'loader', component: LoaderComponent},
     {path:'dice', component:DiceComponent},
 ];
