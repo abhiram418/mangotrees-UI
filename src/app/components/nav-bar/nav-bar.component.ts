3495ed65b7caff49fbb06cb2e4aa0ad1abb8eb52
Abhiram <abhiram.ch418@gmail.com>
2024-06-06 00:21:39 +0530

    account_list_view added

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,9 +1,10 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [NgFor,NgIf],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
@@ -11,4 +12,22 @@ export class NavBarComponent {
   UserName:string="Abhiram";
   CityName:string="Nuzivid";
   Pincode:string="521201";
+
+  account_list:string[]=["Your Account","Your Orders","Your Recomendations"]
+  account_list_show:boolean=false;
+
+
+  show_account(){
+    console.log(this.account_list_show);
+    this.account_list_show=true;
+  }
+  hide_account(){
+      console.log(this.account_list_show);
+    this.account_list_show=false;
+  }
+  hide_account1(){
+    alert('hide1');
+    // console.log(this.account_list_show);
+    // this.account_list_show=false;
+  }
 }
