3042895a3ea23efa0cbb3ad5cb9ad44856c70500
Abhiram <abhiram.ch418@gmail.com>
2024-06-26 10:35:49 +0530

    added login checks

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,29 +1,57 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router} from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { LoaderComponent } from "../components/loader/loader.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-login-page',
     standalone: true,
     templateUrl: './login-page.component.html',
     styleUrl: './login-page.component.css',
-    imports: [NgIf, RouterLink, LoaderComponent]
+    imports: [NgIf, RouterLink, LoaderComponent, ReactiveFormsModule]
 })
 export class LoginPageComponent {
+  LoginForm!: FormGroup;
   ShowCode:boolean=false;
   loader:boolean=false;
 
-  constructor(private router:Router){}
+  constructor(private router:Router){
+    this.CreateForms();
+  }
   
-  code(){
+  CreateForms(){
+    this.LoginForm = new FormGroup({
+      UserName: new FormControl('', [Validators.required,Validators.minLength(4)]),
+      Password: new FormControl('', [Validators.required,Validators.minLength(3)]),
+      SecretKey: new FormControl('')
+    });
+  }
+
+  SecretCode(){
     this.ShowCode = !this.ShowCode;
   }
 
 
   LogIn(){
+    console.log(this.LoginForm.value);
+    
     this.loader=true;
+    if(this.LoginForm.invalid){
+      alert("Please Enter the Credentials");
+      this.loader=false;
+    }
+    else{
+      if(this.LoginForm.get('UserName')?.value == "abhiram"){
+        this.loader=false;
+        this.router.navigate(["../home"]);
+      }
+      else{
+        this.loader=false;
+        alert("Wrong Credentials");
+      }
+    }
   }
 
 }
