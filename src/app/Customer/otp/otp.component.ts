94cd5ca27af09e9acebd806742fe61f36e92425d
Abhiram <abhiram.ch418@gmail.com>
2024-08-11 15:15:24 +0530

    removing the existing login and signup page

--- src/app/Customer/otp/otp.component.ts
+++ src/app/Customer/otp/otp.component.ts
@@ -3,8 +3,6 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Router} from '@angular/router';
 import { LoaderComponent } from "../../components/loader/loader.component";
-import { CustomerSigninService } from '../../Services/customer-signin.service';
-import { ResetPasswordService } from '../../Services/reset-password.service';
 
 @Component({
     selector: 'app-otp',
@@ -14,102 +12,5 @@ import { ResetPasswordService } from '../../Services/reset-password.service';
     imports: [ReactiveFormsModule, RouterLink, LoaderComponent]
 })
 export class OTPComponent{
-  OTPForm!: FormGroup;
-  CustomerOTP:number | null = null;
-  loader:boolean=false;
-  PageName:string | null=null;
 
-  constructor(private router: Router,private snap: ActivatedRoute, private customerSigninService:CustomerSigninService, private resetPasswordService:ResetPasswordService){
-    this.CreateForms();
-    this.GetPageName()
-    this.CheckForTheValue()
-  }
-
-  CreateForms(){
-    this.OTPForm = new FormGroup({
-      otp: new FormControl('', [Validators.required,Validators.minLength(6)])
-    });
-  }
-
-  GetPageName(){
-    this.PageName = this.snap.snapshot.paramMap.get('PageName');
-  }
-
-  CheckForTheValue(){
-    if(this.PageName == null){
-      this.router.navigate(["/login"]);
-    }
-    else{
-      if(this.PageName == "Reset Password"){
-        this.CustomerOTP = this.resetPasswordService.GetOTP();
-        if(!(this.CustomerOTP)){
-          this.router.navigate(["/login"]);
-        }
-      }
-      else{
-        this.CustomerOTP = this.customerSigninService.GetOTP();
-        if(!(this.CustomerOTP)){
-          this.router.navigate(["/signup"]);
-        }
-      }
-    }
-  }
-
-
-
-  verifyOTP(){
-    this.loader=true;
-
-    if(this.OTPForm.invalid){
-      this.loader=false;
-      alert("Not a valid OTP");
-    }
-    else{
-      if(this.PageName == "Reset Password"){
-        this.PostPasswordData();
-      }
-      else{
-        this.PostUserData();
-      }
-    }
-  }
-
-  PostUserData(){
-    if(this.OTPForm.get('otp')?.value==this.CustomerOTP){
-      let posted = this.customerSigninService.PostUserData();
-      if(posted){
-        this.customerSigninService.ClearAll();
-        this.router.navigate(["/login"]);
-      }
-      else{
-        alert("Error occurred try again later");
-        this.customerSigninService.ClearAll();
-        this.router.navigate(["/signup"]);
-      }
-    }
-    else{
-      this.loader=false;
-      alert("Wrong OTP Entered");
-    }
-  }
-
-  PostPasswordData(){
-    if(this.OTPForm.get('otp')?.value==this.CustomerOTP){
-      let posted = this.resetPasswordService.PostPasswordData();
-      if(posted){
-        this.resetPasswordService.ClearAll();
-        this.router.navigate(["/login"]);
-      }
-      else{
-        alert("Error occurred try again later");
-        this.resetPasswordService.ClearAll();
-        this.router.navigate(["/repassword"]);
-      }
-    }
-    else{
-      this.loader=false;
-      alert("Wrong OTP Entered");
-    }
-  }
-  
 }
