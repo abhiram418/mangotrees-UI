b790ffecfe8ed4a3f1856f54177d2e0b4cc04d78
Abhiram <abhiram.ch418@gmail.com>
2024-09-05 23:51:55 +0530

    changed the css for the cart-page

--- src/app/Product/cart-page/cart-page.component.ts
+++ src/app/Product/cart-page/cart-page.component.ts
@@ -1,4 +1,4 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
@@ -7,15 +7,16 @@ import { NavBarData } from '../../components/nav-bar/navBarData';
 @Component({
   selector: 'app-cart-page',
   standalone: true,
-  imports: [NgFor, RouterLink, NavBarComponent],
+  imports: [NgIf, NgFor, RouterLink, NavBarComponent],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css'
 })
 export class CartPageComponent {
   navBarData:NavBarData = new NavBarData();
   count:number = 1;
-  templist:any[] = [1,23,4,5,5,6,7,8,8,9,9,99,2,9,9];
+  templist0:any[] = [1,23,4,5,5,6,7,8,8,9,9,99,2,9,9];
   templist1:any[] = [1,2,3];
+  templist:any[] = [];
 
   constructor(private router:Router){
 
@@ -28,7 +29,9 @@ export class CartPageComponent {
   countDecrease(){
     this.count--;
   }
-
+  clearCart(){
+    this.templist = [];
+  }
   ContinueButtonClicked(){
     this.router.navigate(["../review"]);
   }
