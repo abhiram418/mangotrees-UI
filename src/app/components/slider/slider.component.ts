6ec4b77822312c9b33d4d7146fede8f7ce606d1d
Abhiram <abhiram.ch418@gmail.com>
2024-10-12 02:52:49 +0530

    changed the slider

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,14 +1,15 @@
-import { NgForOf, NgStyle } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgForOf, NgIf, NgStyle } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [ NgForOf, NgStyle],
+  imports: [NgIf, NgForOf, NgStyle],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.css'
 })
 export class SliderComponent {
+  @ViewChild('slider') scrollableDiv!: ElementRef;
   images: string[] = [
     'https://www.paperboatfoods.com/cdn/shop/files/2022_PB_BANNER_02FEB_02__1000x1000-min_5_11zon_1200x.jpg',
     'https://www.paperboatfoods.com/cdn/shop/files/Teacher_Review_PB_Webiste_Desktop_1800x.jpg',
@@ -16,29 +17,34 @@ export class SliderComponent {
     'https://www.akamai.com/site/im-demo/perceptual-standard.jpg',
     'https://industrialempathy.com/img/remote/ZiClJf-1920w.jpg'
   ];
-  index:number=0;
-  image:string=this.images[0];
 
 
-  right(){
-    if(this.index==this.images.length-1){
-      this.image=this.images[0];
-      this.index=0;
-    }
-    else{
-      this.index++;
-      this.image=this.images[this.index];
+  right() {
+    var scrollWidth = this.scrollableDiv.nativeElement.scrollWidth;
+    var clientWidth = this.scrollableDiv.nativeElement.clientWidth;
+    var scrollLeft = this.scrollableDiv.nativeElement.scrollLeft;
+    console.log(scrollWidth)
+    console.log(clientWidth + scrollLeft);
+    if (scrollLeft + clientWidth + 10 >= scrollWidth) {
+      
+      // If reached the end, go back to the start
+      this.scrollableDiv.nativeElement.scrollTo({ left: 0, behavior: 'smooth' });
+    } else {
+      // Scroll right by the width of the screen
+      this.scrollableDiv.nativeElement.scrollBy({ left: clientWidth, behavior: 'smooth' });
     }
   }
-
-  left(){
-    if(this.index==0){
-      this.index=this.images.length-1;
-      this.image=this.images[this.index];
-    }
-    else{
-      this.index--;
-      this.image=this.images[this.index];
+  
+  left() {
+    var clientWidth = this.scrollableDiv.nativeElement.clientWidth;
+    var scrollLeft = this.scrollableDiv.nativeElement.scrollLeft;
+    
+    if (scrollLeft === 0) {
+      // If at the beginning, go to the end
+      this.scrollableDiv.nativeElement.scrollTo({ left: this.scrollableDiv.nativeElement.scrollWidth, behavior: 'smooth' });
+    } else {
+      // Scroll left by the width of the screen
+      this.scrollableDiv.nativeElement.scrollBy({ left: -clientWidth, behavior: 'smooth' });
     }
   }
 }
