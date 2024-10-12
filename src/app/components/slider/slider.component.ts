78e951848e95c05dd6cfe8131a5e64a070a88d8b
Abhiram <abhiram.ch418@gmail.com>
2024-10-12 21:25:24 +0530

    repaired slider

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,5 +1,5 @@
 import { NgForOf, NgIf, NgStyle } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-slider',
@@ -10,6 +10,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class SliderComponent {
   @ViewChild('slider') scrollableDiv!: ElementRef;
+  scrolling: boolean = false;
   images: string[] = [
     'https://www.paperboatfoods.com/cdn/shop/files/2022_PB_BANNER_02FEB_02__1000x1000-min_5_11zon_1200x.jpg',
     'https://www.paperboatfoods.com/cdn/shop/files/Teacher_Review_PB_Webiste_Desktop_1800x.jpg',
@@ -18,33 +19,72 @@ export class SliderComponent {
     'https://industrialempathy.com/img/remote/ZiClJf-1920w.jpg'
   ];
 
+  ngOnInit() {
+    // Check if 'window' is available before accessing it
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', this.onResize.bind(this));
+    }
+  }
+
+  ngOnDestroy() {
+    // Remove the event listener when the component is destroyed
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('resize', this.onResize.bind(this));
+    }
+  }
+
+  onResize() {
+    const clientWidth = this.scrollableDiv.nativeElement.clientWidth;
+    const scrollLeft = this.scrollableDiv.nativeElement.scrollLeft;
+
+    // Calculate which image is currently being viewed
+    const currentImageIndex = Math.round(scrollLeft / clientWidth);
+
+    // Scroll back to the closest image after resizing
+    this.scrollableDiv.nativeElement.scrollTo({
+      left: currentImageIndex * clientWidth,
+      behavior: 'auto'
+    });
+  }
+  
+
 
   right() {
+    if (this.scrolling) return;  
+    this.scrolling = true;
+
     var scrollWidth = this.scrollableDiv.nativeElement.scrollWidth;
     var clientWidth = this.scrollableDiv.nativeElement.clientWidth;
     var scrollLeft = this.scrollableDiv.nativeElement.scrollLeft;
+
     console.log(scrollWidth)
     console.log(clientWidth + scrollLeft);
     if (scrollLeft + clientWidth + 10 >= scrollWidth) {
-      
-      // If reached the end, go back to the start
       this.scrollableDiv.nativeElement.scrollTo({ left: 0, behavior: 'smooth' });
     } else {
-      // Scroll right by the width of the screen
       this.scrollableDiv.nativeElement.scrollBy({ left: clientWidth, behavior: 'smooth' });
     }
+
+    setTimeout(() => {
+      this.scrolling = false;  
+    }, 1000);
   }
   
   left() {
+    if (this.scrolling) return; 
+    this.scrolling = true;
+
     var clientWidth = this.scrollableDiv.nativeElement.clientWidth;
     var scrollLeft = this.scrollableDiv.nativeElement.scrollLeft;
     
     if (scrollLeft === 0) {
-      // If at the beginning, go to the end
       this.scrollableDiv.nativeElement.scrollTo({ left: this.scrollableDiv.nativeElement.scrollWidth, behavior: 'smooth' });
     } else {
-      // Scroll left by the width of the screen
       this.scrollableDiv.nativeElement.scrollBy({ left: -clientWidth, behavior: 'smooth' });
     }
+
+    setTimeout(() => {
+      this.scrolling = false;  
+    }, 1000);
   }
 }
