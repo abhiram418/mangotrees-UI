bffdfe47b119a0d069faacbbd4c03d2d2fe93c0d
Abhiram <abhiram.ch418@gmail.com>
2024-10-27 20:35:28 +0530

    simple

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { CustomerSigninService } from './Services/customer-signin.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration()]
